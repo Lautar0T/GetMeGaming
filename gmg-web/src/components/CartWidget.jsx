e7705e83fd2358e525c0ce7099fe9c7d9e89a9e9
lautaroturic <lautaroturic@gmail.com>
2022-05-17 21:09:42 -0300

    desafio 6

--- gmg-web/src/components/CartWidget.jsx
+++ gmg-web/src/components/CartWidget.jsx
@@ -3,7 +3,7 @@ import {ImCart} from 'react-icons/im'
 const CartWidget = () => {
     return (
         <li className='flex cursor-pointer' onClick={() => {console.log("Carrito")}}>
-            <ImCart className=' z-10 '/>
+            <ImCart className=' z-10 text-slate-300 hover:text-slate-500 '/>
             <div className=' rounded-lg h-4 w-4 bg-red-600 text-sm text-center font-extrabold -translate-y-2 '><p className='-translate-y-0.5'>!</p></div>
         </li>
     )
--- gmg-web/src/components/NavBar.jsx
+++ gmg-web/src/components/NavBar.jsx
@@ -7,12 +7,12 @@ const NavBar = () => {
     return (
         <nav className="flex justify-between  items-center p-4 fixed top-0 left-0 right-0 z-30 bg-gray-900 border-2 border-gray-800">
             <div><a href="/" className="text-4xl font-bold flex items-end">
-                <p className='text-violet-600 hover:text-violet-800'>Get</p>
-                <p className='text-slate-300  hover:text-slate-500'>Me</p>
-                <p className='text-violet-600 hover:text-violet-800'>Gaming</p>
+                <p className='bg-violet-600 hover:bg-violet-800 text-gray-900 rounded-lg rounded-r-none shadow-md shadow-violet-800 px-1'>Get</p>
+                <p className='bg-slate-300  hover:bg-slate-500 text-gray-900 shadow-md shadow-slate-500 px-1'>Me</p>
+                <p className='bg-violet-600 hover:bg-violet-800 text-gray-900 rounded-lg rounded-l-none  shadow-md shadow-violet-800 px-1'>Gaming</p>
                 <p className=' text-lg text-slate-300  hover:text-slate-500'>.com</p>
             </a></div>
-            <ul className="lg:flex text-xl justify-end w-2/5 items-center pr-10 hidden">
+            <ul className="lg:flex text-xl justify-end w-2/5 items-center pr-10 hidden ">
                 <NavItem cat={categorias[0]} />
                 <NavItem cat={categorias[1]} />
                 <NavItem cat={categorias[2]} />
