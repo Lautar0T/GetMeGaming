1f050a2ad6a09b27d20de236434c196ef626a1a1
lautaroturic <lautaroturic@gmail.com>
2022-05-31 22:49:15 -0300

    lang

--- gmg-web/src/components/Cart.jsx
+++ gmg-web/src/components/Cart.jsx
@@ -6,9 +6,13 @@ const Cart = () => {
     const { cartList, cartTotal, clearCart, removeFromCart, emptyCart, showCart } = useCartContext()
     const navigate = useNavigate()
     showCart()
-    function orderGenerator(){
+    function langForm(x) {
+        return x.toLocaleString('es-AR');
+    }
+
+    function orderGenerator() {
         let order = {}
-        order.buyer = {name: "John",lastname: "Doe", email: "john_doe@gmail.com", phone: "1144235622"}
+        order.buyer = { name: "John", lastname: "Doe", email: "john_doe@gmail.com", phone: "1144235622" }
         order.total = cartTotal
         order.items = cartList.map(item => {
             return {
@@ -18,19 +22,19 @@ const Cart = () => {
                 quantity: item.cantidad
             }
         })
-        console.log( 'order', order)
+        console.log('order', order)
     }
     orderGenerator()
     return (
         <>
             <IoReturnUpBack className=' justify-self-start px-2 text-white w-12 h-12 rounded-lg ' onClick={() => navigate(-1)} />
             <div className="text-slate-100 grid p-3 lg:p-6 gap-2 bg-gray-700 border-gray-800 border-2 rounded-lg">
-                <table className={emptyCart ? "table-auto rounded-t-lg bg-slate-500 max-w-[90%]" : "table-auto rounded-lg bg-slate-500 "}>
+                <table className={emptyCart ? "table-fixed  rounded-t-lg bg-slate-500 " : "table-auto rounded-lg bg-slate-500 "}>
                     <thead>
                         <tr className=" border-b-2 border-slate-400 font-semibold lg:text-xl content-between">
-                            <th  className="pl-2 p-1 lg:pr-24">Articulo</th>
+                            <th className="pl-2 p-1 lg:pr-24">Articulo</th>
                             <th className="p-1 lg:pr-24">Cantidad</th>
-                            <th className="p-1 lg:pr-24">Precio</th>
+                            <th className="p-1 lg:pr-24">Precio /u</th>
                             <th className="p-1 lg:pr-24">Subtotal</th>
                             <th className="p-1 lg:pr-12">Eliminar</th>
                         </tr>
@@ -41,8 +45,8 @@ const Cart = () => {
                                 <tr key={item.id} className="bg-slate-600 border-slate-400 p-2 border-b-2 text-center text-lg">
                                     <td className="pl-2 p-1 lg:pr-24 font-medium">{item.name}</td>
                                     <td className="p-1 lg:pr-24">{item.cantidad}</td>
-                                    <td className="p-1 lg:pr-24">${(item.price)}</td>
-                                    <td className="p-1 lg:pr-24">${(item.price * item.cantidad)}</td>
+                                    <td className="p-1 lg:pr-24">${langForm(item.price)}</td>
+                                    <td className="p-1 lg:pr-24">${langForm(item.price * item.cantidad)}</td>
                                     <td className="justify-center flex items-baseline p-1 lg:pr-12"><MdDeleteOutline className="text-red-600 hover:text-red-700 w-full h-7 " onClick={() => removeFromCart(item)} /></td>
                                 </tr>
                             ))}
@@ -50,7 +54,7 @@ const Cart = () => {
                     </>
                 </table>
                 {emptyCart ? <div className="text-center grid "><span className=" rounded-b-lg py-1 text-lg font-medium bg-slate-500 ">El Carrito esta Vacio...</span><Link to={'/'} className='bg-violet-800 justify-self-center p-1 mt-2 rounded-lg' >Ver Productos</Link></div> : <section className="flex justify-end gap-2">
-                    <p className="text-lg">Total: ${cartTotal}</p>
+                    <p className="text-xl font-medium">Total: ${langForm(cartTotal)}</p>
                     <Link to={"/"} className="rounded-lg bg-slate-400 p-1 font-medium" >Seguir Comprando</Link>
                     <button className="bg-red-600 rounded-lg px-1 flex items-center font-medium" onClick={() => clearCart()} >
                         <p>Vaciar</p> <MdDeleteForever className="w-5 h-5" />
--- gmg-web/src/components/ItemList.jsx
+++ gmg-web/src/components/ItemList.jsx
@@ -1,11 +1,14 @@
 import Item from "./Item"
 
 const ItemList = ({ items, loading }) => {
+    function langForm(x) {
+        return x.toLocaleString('es-AR');
+    }
     return (
         <>
             <div className={loading ? "flex justify-center" : "grid md:grid-cols-2 xl:grid-cols-3 gap-4 place-content-stretch justify-center pt-3"}>
                 
-                {items.map(item => <Item key={item.id} prodId={item.id} name={item.name} price={item.price} img={item.img} stock={item.stock} descr={item.description} />)}
+                {items.map(item => <Item key={item.id} prodId={item.id} name={item.name} price={langForm(item.price)} img={item.img} stock={langForm(item.stock)} descr={item.description} />)}
             </div>
         </>
     )
